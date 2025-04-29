0d70cacf1e958ed5b3986dd3e02e272bac412352
milesdev110 <milesbennett076@gmail.com>
2025-04-29 10:43:20 +0000

    Optimization the prompt.
    Signed-off-by: milesdev110 <milesbennett076@gmail.com>

--- packages/protocols/src/index.ts
+++ packages/protocols/src/index.ts
@@ -53,7 +53,6 @@ export const handleProtocols = async (runtime: any, originText: any) => {
     // console.log("handleProtocols res0: ", res0);
     // 1 token is about 4 chars.
     const charLengthLimit = 128000 * 3;
-    let resStr = "";
     const apiXDataSourceArray = await runtime.cacheManager.get(
         "XData_Collection"
     );
@@ -74,33 +73,42 @@ export const handleProtocols = async (runtime: any, originText: any) => {
     const response1 = await generateText({
         runtime,
         context: shortenStr(promt1),
-        modelClass: ModelClass.SMALL,
+        modelClass: ModelClass.LARGE,
     });
     console.log("handleProtocols response1: ", response1);
     const response1Str = response1.replace(/```json/g, "").replace(/```/g, "");
 
     let obj = JSON.parse(response1Str);
-    let currentAPIResStr = "";
-    let aipartStr = `AI part:
-[Part 1: ...]
-[Part 2: ...]
-[Part 3: ]
-[Part 4: ]
-[Part 5: ]
-[Part 6: ]
-[Part 7: ]`;
- let   originQuestionPrompt = `
-You are a data AI proxy. My following prompt is divided into two parts. The first part is the thinking template I provide for you. The second part is where I append your output as results to the end of the template. During this process, I allow you to access network data. With each loop, you continuously request network data, and ultimately answer the user's question.
-Template part:
-[Part 1 Role: You are a data AI proxy that answers some user questions by querying network data multiple times.]
-[Part 2 Original question: ${originText}]
-[Part 3 Plan: 1.xxx. 2.xxx. (Clarify the core points of the question (e.g., the definition and scope of xxx).)]
-[Part 4: Steps: 1.xxx. 2.xxx. (You can record the simple situation and feedback in each step here. The feedback may be positive or negative to help you formulate the next step plan.)]
-[Part 5: Collected results: xxxx (This is a step-by-step result. Your each network request is processed, refined, and collected here. As you collect more and more data, when you have enough data, you can answer the user's question.)]
-[Part 6: Next step plan: xxx (Adjust your plan continuously based on the data you collect.)]
-[Part 7: The temporary results of network requests are organized and refined, and then merged into Part 5.]
+
+let promptPartOne = `
+You are a data AI Agent that answers some user questions by querying network data multiple times.
+[User Original question: ${originText}]
+To make it easier for you to perform long logical inferences, I have mapped out the inference areas in your context.
+This is a dynamic reasoning graph that includes the original question, each network request, and the new data collected at each step, as well as future plans, summaries, and reflections.
+You Context is divided into three areas, and each area has some blocks.
+[The first area] is the thinking template I provide for you.
+[The second area] is your output according first area. With each loop, Before you thinking, I allow you to access network data until have enough data to ultimately answer the user's question.
+[The third area] is the statement that you and I interact with at the current step and current state.
+
+[Area1] :
+[Block 1: Plan Block: 1.xxx. 2.xxx. (Clarify the core points of the question (e.g., the definition and scope of xxx).)]
+[Block 2: Steps Logs Block: 1.xxx. 2.xxx. (You can record the simple situation and feedback in each step here. The feedback may be positive or negative to help you formulate the next step plan.)]
+[Block 3: Data Collected Block: data1, data2, ... (This is a step-by-step result. Your each network request is processed, refined, and collected here. As you collect more and more data, when you have enough data, you can answer the user's question.)]
+[Block 4: Next step plan Block: xxx (Adjust your plan continuously based on the data you collect.)]
+[Block 5: Temporary Block: (1) data1, The temporary results of network requests are organized and refined, and then merged into Part 3. (2) data2, The current output needs to be saved in the current block for the next step of input.
+For example, the return value next_cursor is used as the cursor parameter to complete the page query when turning pages.]
 \n`;
-let chatContextAccmulatingStr = originQuestionPrompt + aipartStr;
+let promptPartTwo = `[Area2]:
+[Block 1: ...]
+[Block 2: ...]
+[Block 3: ]
+[Block 4: ]
+[Block 5: ]`;
+
+let promptPartThree = `
+\n[Area3]:
+`; // [The third area] 
+
 
 
     let step = 0;
@@ -112,108 +120,111 @@ let chatContextAccmulatingStr = originQuestionPrompt + aipartStr;
             break;
         }
         console.log(`handleProtocols step: ${step} begin -------------------------------------`);
+        let chatContextAccmulatingStr = promptPartOne + promptPartTwo + promptPartThree;
 
         if(chatContextAccmulatingStr.length > charLengthLimit) {
-            chatContextAccmulatingStr = chatContextAccmulatingStr.slice(charLengthLimit);
+            console.log(`handleProtocols step: ${step} chatContextAccmulatingStr is too much, break.`);
             break; // context length limit 128k for GPT-4.
         }
 
         if (!obj?.need_network.includes("true")) {
+            // STEP: No need to query network data.
             break;
         }
-        // STEP: Data source selection
-        const promt2 = chatContextAccmulatingStr + `. And you need to call multiple data request interfaces to answer user questions. The following answer should be as tight and brief as possible.
-        Please analyze the description of the interface below. ${JSON.stringify(
+
+        // STEP: Data Query API selection
+        promptPartThree = `\n[Area3]\nYou need to call multiple data request interfaces to answer user questions. The following answer should be as tight and brief as possible.
+        Please analyze the description of the API below. ${JSON.stringify(
             apiXDataSourceArray
         )} , 
         Extracting the parameters of the problem from the user's question, ${originText}, 
-        Retrieve from the interface description in other parameters. 
+        Retrieve from the API description in other parameters. 
         Please return a JSON object containing the following fields:
         {"baseurl": "url_1", "method": "{post or get}", "params": "object_1", "headers": "object_1", "body": "object_1"}, 
         When you use an API to search for multiple keywords, you can split them up, such as: Query(A), Query(B), Query(C). Instead of just Query(A B C), it is easy to get no results if you query many groups of keywords at the same time.There is another situation where there is a dependency relationship, and this time you only need to return the interface of the current data. I will add the return result of this data query and origin question text in the next loop. Based on the new return result, you can continue to select the API for network calls and complete the dependency calls.
-        The complete calling process is similar to the following:
-        [Question: user origin question text]
-        [Step1]:
-        HTTP query Url1,
-        HTTP responce ans1,
-        [Step2]:
-        HTTP query Url2
-        HTTP responce ans2
-        [Step3]:
-        HTTP query Url3
-        HTTP responce ans3
+        At the same time, you also need to retrieve the parameters that may be used in [Area2][Block 5], such as the next_cursor in the previous return value, and pass it in as the cursor parameter for page turning query.
+        Only one http request interface is returned at a time, and subsequent data can be requested in subsequent loops.
         Please return this JSON object directly without any explanation, comments, other content, or markdown syntax modification.`;
-        console.log("handleProtocols promt2: ", promt2);
-        if(promt2.length > charLengthLimit) {
+
+        chatContextAccmulatingStr = promptPartOne + promptPartTwo + promptPartThree;
+        if(chatContextAccmulatingStr.length > charLengthLimit) {
+            console.log(`handleProtocols step: ${step} promt2 is too much, break.`);
             return chatContextAccmulatingStr;
         }
         const response2 = await generateText({
             runtime,
-            context: shortenStr(promt2),
+            context: shortenStr(chatContextAccmulatingStr),
             modelClass: ModelClass.LARGE,
         });
         console.log("handleProtocols response2: ", response2);
         const response2Str = response2.replace(/```json/g, "").replace(/```/g, "");
         const Obj = JSON.parse(response2Str);
 
-        // chatContextAccmulatingStr += `\n[Current step: ${step}]\n`;
-
-        // for (const Obj of ObjArray) {
-            let apires = null;
-            let currentApiStr = "";
-            try {
-                if (Obj.method == "post") {
-                    apires = await axios.post(Obj.baseurl, Obj.body, {
-                        params: Obj.params,
-                        headers: Obj.headers,
-                    });
-                } else {
-                    apires = await axios.get(Obj.baseurl, {
-                        params: Obj.params,
-                        headers: Obj.headers,
-                    });
-                }
-                currentApiStr = `The current API [API: ${JSON.stringify(Obj)}] The responce [Responce: ${JSON.stringify(apires.data)}].\n`;
-                //if(currentApiStr.length > charLengthLimit / 10) {
-                    let apiNeedShortenStr = `The user origin quesiton[Question: ${originText}], The current API [API: ${JSON.stringify(Obj)}] The response str is too long, Based on the user's question, please remove irrelevant text, remove duplicate text and compress responce.For example, if the user's question is not related to the timestamp, the timestamp field can be removed. [Responce: ${JSON.stringify(apires.data)}].\n`;
-                    // The response str is too long, Use AI to remove irrelevant text and compress it.
-                    const promtShorten = apiNeedShortenStr;
-                    const shortenapires = await generateText({ // Compress redundant and irrelevant text
-                        runtime,
-                        context: shortenStr(promtShorten),
-                        modelClass: ModelClass.LARGE,
-                    });
-                    currentApiStr = `The current API [API: ${JSON.stringify(Obj)}] The responce [Responce: ${shortenapires}].\n`;
-                    // console.log("handleProtocols promtShorten: ", promtShorten);
-                    // console.log("handleProtocols currentApiStr: ", currentApiStr);
-                //}
-            } catch (e) {
-                console.log("handleProtocols error: ", e);
-                const errorStr =
-                    `Current API [${JSON.stringify(Obj)}]  request failed, please check if the network connection or API parameters are correct.`;
-                // chatContextAccmulatingStr += errorStr;
-                currentApiStr = `The current API [API: ${JSON.stringify(Obj)}] The responce [Responce: ${errorStr}].\n`;
+        let apires = null;
+        let currentApiStr = "";
+        try {
+            if (Obj.method == "post") {
+                apires = await axios.post(Obj.baseurl, Obj.body, {
+                    params: Obj.params,
+                    headers: Obj.headers,
+                });
+            } else {
+                apires = await axios.get(Obj.baseurl, {
+                    params: Obj.params,
+                    headers: Obj.headers,
+                });
             }
-            currentAPIResStr += currentApiStr;
+            promptPartThree = `\n[Area3]\nThe user origin quesiton[Question: ${originText}], The current API [API: ${JSON.stringify(
+                Obj
+            )}] The response str is too long, Based on the user's question, please remove irrelevant text, remove duplicate text and compress responce. For example, if the user's question is not related to the timestamp, the timestamp field can be removed. 
+            Some fields are not related to the question and cannot be removed, such as next_cursor used for paging query, which needs to be used as the value of cursor as a parameter in the next query to complete the paging query.
+            [Responce: ${JSON.stringify(
+                apires.data
+            )}].\n`;
+            // The response str is too long, Use AI to remove irrelevant text and compress it.
+            // const promtShorten = apiNeedShortenStr;
+            const shortenapires = await generateText({
+                // Compress redundant and irrelevant text
+                runtime,
+                context: shortenStr(promptPartOne + promptPartTwo + promptPartThree),
+                modelClass: ModelClass.LARGE,
+            });
+            currentApiStr = `The current API [API: ${JSON.stringify(
+                Obj
+            )}] The responce [Responce: ${shortenapires}].\n`;
+            // console.log("handleProtocols promtShorten: ", promtShorten);
+            // console.log("handleProtocols currentApiStr: ", currentApiStr);
+            //}
+        } catch (e) {
+            console.log("handleProtocols error: ", e);
+            // chatContextAccmulatingStr += errorStr;
+            currentApiStr = `The current API [API: ${JSON.stringify(
+                Obj
+            )}] request failed, The responce [Responce: ${e.toString().slice(100)}].\n`;
+        }
+        // currentAPIResStr += currentApiStr;
 
-            console.log(
-                "handleProtocols http res: ",
-                JSON.stringify(apires?.data)?.slice(0, 1000)
-            );
+        console.log(
+            "handleProtocols http res: ",
+            JSON.stringify(apires?.data)?.slice(0, 1000)
+        );
         // }
-        chatContextAccmulatingStr += `[PART 7: ${currentAPIResStr}]\n`;
+        promptPartTwo += `[Block 5: ${currentApiStr}]\n`;
+        promptPartThree = `\n[Area3]\nPlease summarize the AI PART 5 to AI PART 3, and then Return Second Area(AI Area)`;
 
 
-        chatContextAccmulatingStr = await generateText({
+        promptPartTwo = await generateText({
             runtime,
-            context: shortenStr(chatContextAccmulatingStr + "\nPlease summarize the AI PART 7 to AI PART 5"),
+            context: shortenStr(promptPartOne + promptPartTwo + promptPartThree),
             modelClass: ModelClass.LARGE,
         });
 
         console.log(
             `handleProtocols chatContextAccmulatingStr: ${chatContextAccmulatingStr}`
         );
-        const promt3 = `${chatContextAccmulatingStr} // Check if the goal is completed
+
+        // Check if the goal is completed
+        promptPartThree = `\n[Area3]\n
         Please check if the data above is sufficient to answer the user's question?\n
         You can read this list of HTTP APIs [APIs: ${JSON.stringify(
             apiXDataSourceArray
@@ -221,10 +232,9 @@ let chatContextAccmulatingStr = originQuestionPrompt + aipartStr;
         If the collected data is not enough, you need to continue searching online.You need to continue collecting data until the problem is finally solved. For example, if a user needs to find 100 KOLs, but you only find 10, this is not enough.
         Please return a Boolean value, true or false, to indicate whether this issue needs to be queried online.\n
         Please use JSON to return the result, without including any other content or requiring markdown syntax modification, The JSON format is: {"need_network": "true"}\n`;
-        console.log(`handleProtocols promt3: ${promt3}`);
         const response3 = await generateText({
             runtime,
-            context: shortenStr(promt3),
+            context: shortenStr(promptPartOne + promptPartTwo + promptPartThree),
             modelClass: ModelClass.LARGE,
         });
         console.log(`handleProtocols response3: ${response3}`);
@@ -242,9 +252,10 @@ let chatContextAccmulatingStr = originQuestionPrompt + aipartStr;
 
 
     } while (obj);
+    promptPartThree = "\n[Area3]\nBased on the above results, answer user questions briefly and directly. If there is not enough data to answer some questions first, you also need to answer some questions first."
     const responseFinal = await generateText({
         runtime,
-        context: shortenStr(chatContextAccmulatingStr + "\nBased on the above results, please answer the user's question."),
+        context: shortenStr(promptPartOne + promptPartTwo + promptPartThree),
         modelClass: ModelClass.LARGE,
     });
     return responseFinal;
