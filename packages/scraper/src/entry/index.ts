20e7b3bf42cbd2a7ea4e1a9c199f6e8214551a61
milesdev110 <milesbennett076@gmail.com>
2025-05-20 11:00:49 +0000

    Add catch for health check.
    Signed-off-by: milesdev110 <milesbennett076@gmail.com>

--- packages/scraper/src/entry/index.ts
+++ packages/scraper/src/entry/index.ts
@@ -59,7 +59,6 @@ class Server {
                 }
             } catch (error) {
                 console.error("Error during search:", error);
-                throw error;
             }
             return "error";
         }
@@ -79,61 +78,65 @@ class Server {
                 }
             } catch (error) {
                 console.error("Error during search:", error);
-                throw error;
             }
             return "error";
         }
         if (item_name === "topic_rank") {
-            const response = await axios.post(
-                "https://gw.newrank.cn/api/xh/xdnphb/nr/app/xhs/rank/topicRank",
-                {
-                    type: "\u5168\u90E8",
-                    dateType: 1,
-                    sort: "interactiveCount",
-                    rankDate: "2025-05-19",
-                    size: 20,
-                    start: 1,
-                },
-                {
-                    headers: {
-                        "n-token": "35c430ef650b459ba2b9c1409148d929",
-                        request_id: "05c9a9ad73d64850a087b03bcf4d8b50",
-                        Cookie: "tfstk=gTxnMOGrRe7IM6RY9CSIo3eA28gOAJs5kQERwgCr715_wQpyeuVyEQbRvLlBrCjBXeER9XskUIIoMjnxDp9CAglxMvu71SIVH_WUw6rwQgBoJ0tqMp9CVR90-rBed8j7VkJP4QSN7tX54yrPaAbNHTSzYarU7551UgSP8TPa7t6VU95P4AvN1TSPaQSr9NZPi3-6bfTQWZJWjItGKwf2IVZ8VyWUMs9FOurPt9bhSp5g4u-MrwvbL6ubB_sdOLWHNmZCYaYyvG-E_0S2lCxlui0-divMWEQHSXrl6HLMjMKENr99I_Rl4eM7_ddAtESBe8GwIZvyVN8EskvCjttPu3Vn6_LvBH7MgfEMNaYyvG-E_mjrPPzVSxr5QYKaPz_FCOfAijAkA_Y1lLkiIrgC8O6KMADgPW7FCOXiIA44VwW1L8C..; token=4C4729530477498BAEEC97859C380B26; acw_tc=0a472f4317477286944042667e007e879955147b13f7ce1efeee0e489c211f; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22nr_my2b9az6y%22%2C%22first_id%22%3A%2219641461076860-0e9b6d1278a91f-26011c51-2073600-19641461077b29%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk2NDE0NjEwNzY4NjAtMGU5YjZkMTI3OGE5MWYtMjYwMTFjNTEtMjA3MzYwMC0xOTY0MTQ2MTA3N2IyOSIsIiRpZGVudGl0eV9sb2dpbl9pZCI6Im5yX215MmI5YXo2eSJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22nr_my2b9az6y%22%7D%7D; auth_n=37ZQoBZfU8kqgB463MGBXeOTQMzOnuqmpuQNP0iNQ58JDXxtcb44312GIfD0ZsQV",
+            try {
+                const response = await axios.post(
+                    "https://gw.newrank.cn/api/xh/xdnphb/nr/app/xhs/rank/topicRank",
+                    {
+                        type: "\u5168\u90E8",
+                        dateType: 1,
+                        sort: "interactiveCount",
+                        rankDate: "2025-05-19",
+                        size: 20,
+                        start: 1,
                     },
+                    {
+                        headers: {
+                            "n-token": "35c430ef650b459ba2b9c1409148d929",
+                            request_id: "05c9a9ad73d64850a087b03bcf4d8b50",
+                            Cookie: "tfstk=gTxnMOGrRe7IM6RY9CSIo3eA28gOAJs5kQERwgCr715_wQpyeuVyEQbRvLlBrCjBXeER9XskUIIoMjnxDp9CAglxMvu71SIVH_WUw6rwQgBoJ0tqMp9CVR90-rBed8j7VkJP4QSN7tX54yrPaAbNHTSzYarU7551UgSP8TPa7t6VU95P4AvN1TSPaQSr9NZPi3-6bfTQWZJWjItGKwf2IVZ8VyWUMs9FOurPt9bhSp5g4u-MrwvbL6ubB_sdOLWHNmZCYaYyvG-E_0S2lCxlui0-divMWEQHSXrl6HLMjMKENr99I_Rl4eM7_ddAtESBe8GwIZvyVN8EskvCjttPu3Vn6_LvBH7MgfEMNaYyvG-E_mjrPPzVSxr5QYKaPz_FCOfAijAkA_Y1lLkiIrgC8O6KMADgPW7FCOXiIA44VwW1L8C..; token=4C4729530477498BAEEC97859C380B26; acw_tc=0a472f4317477286944042667e007e879955147b13f7ce1efeee0e489c211f; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22nr_my2b9az6y%22%2C%22first_id%22%3A%2219641461076860-0e9b6d1278a91f-26011c51-2073600-19641461077b29%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk2NDE0NjEwNzY4NjAtMGU5YjZkMTI3OGE5MWYtMjYwMTFjNTEtMjA3MzYwMC0xOTY0MTQ2MTA3N2IyOSIsIiRpZGVudGl0eV9sb2dpbl9pZCI6Im5yX215MmI5YXo2eSJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22nr_my2b9az6y%22%7D%7D; auth_n=37ZQoBZfU8kqgB463MGBXeOTQMzOnuqmpuQNP0iNQ58JDXxtcb44312GIfD0ZsQV",
+                        },
+                    }
+                );
+                if (response?.data?.data?.list?.length > 0) {
+                    return "success";
                 }
-            );
-
-            if (response?.data?.data?.list?.length > 0) {
-                return "success";
-            } else {
-                return "error";
+            } catch (error) {
+                console.error("Error during search:", error);
             }
+            return "error";
         }
 
         if (item_name === "hot_words") {
-            const response = await axios.post(
-                "https://gw.newrank.cn/api/xhsv2/nr/app/xh/v2/rank/hotWordHotList",
-                {
-                    rankType: "day",
-                    rankDate: "2025-05-19",
-                    size: 20,
-                    start: 1,
-                    sort: "hot_score",
-                },
-                {
-                    headers: {
-                        "n-token": "35c430ef650b459ba2b9c1409148d929",
-                        request_id: "e35a27c0f028425f9a8b96c6b37bab30",
-                        Cookie: "tfstk=gTxnMOGrRe7IM6RY9CSIo3eA28gOAJs5kQERwgCr715_wQpyeuVyEQbRvLlBrCjBXeER9XskUIIoMjnxDp9CAglxMvu71SIVH_WUw6rwQgBoJ0tqMp9CVR90-rBed8j7VkJP4QSN7tX54yrPaAbNHTSzYarU7551UgSP8TPa7t6VU95P4AvN1TSPaQSr9NZPi3-6bfTQWZJWjItGKwf2IVZ8VyWUMs9FOurPt9bhSp5g4u-MrwvbL6ubB_sdOLWHNmZCYaYyvG-E_0S2lCxlui0-divMWEQHSXrl6HLMjMKENr99I_Rl4eM7_ddAtESBe8GwIZvyVN8EskvCjttPu3Vn6_LvBH7MgfEMNaYyvG-E_mjrPPzVSxr5QYKaPz_FCOfAijAkA_Y1lLkiIrgC8O6KMADgPW7FCOXiIA44VwW1L8C..; token=4C4729530477498BAEEC97859C380B26; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22nr_my2b9az6y%22%2C%22first_id%22%3A%2219641461076860-0e9b6d1278a91f-26011c51-2073600-19641461077b29%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk2NDE0NjEwNzY4NjAtMGU5YjZkMTI3OGE5MWYtMjYwMTFjNTEtMjA3MzYwMC0xOTY0MTQ2MTA3N2IyOSIsIiRpZGVudGl0eV9sb2dpbl9pZCI6Im5yX215MmI5YXo2eSJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22nr_my2b9az6y%22%7D%7D; acw_tc=1a0c399717477309968887216e005a44214b0ae8591dccc4e0c0255ba831e2; auth_n=37ZQoBZfU8kqgB463MGBXeOTQMzOnuqmpuQNP0iNQ5+vY+AaKU/ZXPASuRT1eoxl",
+            try {
+                const response = await axios.post(
+                    "https://gw.newrank.cn/api/xhsv2/nr/app/xh/v2/rank/hotWordHotList",
+                    {
+                        rankType: "day",
+                        rankDate: "2025-05-19",
+                        size: 20,
+                        start: 1,
+                        sort: "hot_score",
                     },
-                }
-            );
+                    {
+                        headers: {
+                            "n-token": "35c430ef650b459ba2b9c1409148d929",
+                            request_id: "e35a27c0f028425f9a8b96c6b37bab30",
+                            Cookie: "tfstk=gTxnMOGrRe7IM6RY9CSIo3eA28gOAJs5kQERwgCr715_wQpyeuVyEQbRvLlBrCjBXeER9XskUIIoMjnxDp9CAglxMvu71SIVH_WUw6rwQgBoJ0tqMp9CVR90-rBed8j7VkJP4QSN7tX54yrPaAbNHTSzYarU7551UgSP8TPa7t6VU95P4AvN1TSPaQSr9NZPi3-6bfTQWZJWjItGKwf2IVZ8VyWUMs9FOurPt9bhSp5g4u-MrwvbL6ubB_sdOLWHNmZCYaYyvG-E_0S2lCxlui0-divMWEQHSXrl6HLMjMKENr99I_Rl4eM7_ddAtESBe8GwIZvyVN8EskvCjttPu3Vn6_LvBH7MgfEMNaYyvG-E_mjrPPzVSxr5QYKaPz_FCOfAijAkA_Y1lLkiIrgC8O6KMADgPW7FCOXiIA44VwW1L8C..; token=4C4729530477498BAEEC97859C380B26; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22nr_my2b9az6y%22%2C%22first_id%22%3A%2219641461076860-0e9b6d1278a91f-26011c51-2073600-19641461077b29%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk2NDE0NjEwNzY4NjAtMGU5YjZkMTI3OGE5MWYtMjYwMTFjNTEtMjA3MzYwMC0xOTY0MTQ2MTA3N2IyOSIsIiRpZGVudGl0eV9sb2dpbl9pZCI6Im5yX215MmI5YXo2eSJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22nr_my2b9az6y%22%7D%7D; acw_tc=1a0c399717477309968887216e005a44214b0ae8591dccc4e0c0255ba831e2; auth_n=37ZQoBZfU8kqgB463MGBXeOTQMzOnuqmpuQNP0iNQ5+vY+AaKU/ZXPASuRT1eoxl",
+                        },
+                    }
+                );
 
-            if (response?.data?.data?.list?.length > 0) {
-                return "success";
-            } else {
-                return "error";
+                if (response?.data?.data?.list?.length > 0) {
+                    return "success";
+                }
+            } catch (error) {
+                console.error("Error during search:", error);
             }
+            return "error";
         }
     }
 
