4f8c368c7da7f68ed4f34da186a4575417419fb6
milesdev110 <milesbennett076@gmail.com>
2025-05-24 14:53:43 +0300

    Add hot words API.
    Signed-off-by: milesdev110 <milesbennett076@gmail.com>

--- packages/protocols/src/apiwrapper.ts
+++ packages/protocols/src/apiwrapper.ts
@@ -21,27 +21,66 @@ class APIWrapperFactory {
      */
 
     public static async executeRequest(obj: any , taskId: string): Promise<any | undefined> {
-        // console.log(`executeRequest routeStr: ${routeStr} params: ${paramsStr}`);
-        // let params;
-        // try {
-        //    params = JSON.parse(paramsStr);
-
-        // } catch (error) {
-        //   params = paramsStr;
-        //   console.error("Failed to parse params:", error);
-        // }
         // {"route": "notes_search","params": {"key1": "v1","key2": "v2"}}
-
-        console.log(
-            `executeRequest routeStr: ${obj.route} params: ${JSON.stringify(
-                obj?.params?.noteId
-            )}`
-        );
+        console.log(`executeRequest params: ${JSON.stringify(obj)}`);
         let result;
         switch (obj.route) {
-            // case "hot_words":
-            //   result = this.getInstance().getHotWords(params.page);
-            //   break;
+            case "hot_words":
+             
+    try {
+      const page = obj?.params?.page || 1;
+      console.log("Fetching hot words... page: " + page);
+
+const response = await axios.post(
+  'https://gw.newrank.cn/api/xhsv2/nr/app/xh/v2/rank/hotWordHotList',
+  {
+    'typeV1': '',
+    'typeV2': '',
+    'rankType': 'day',
+    'rankDate': '2025-05-23',
+    'recentType': '',
+    'size': 20,
+    'start': page,
+    'isNew': '',
+    'isBoom': '',
+    'sort': 'hot_score'
+  },
+  {
+    headers: {
+      'Accept': '*/*',
+      'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+      'Connection': 'keep-alive',
+      'Origin': 'https://xh.newrank.cn',
+      'Referer': 'https://xh.newrank.cn/',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-site',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+      'content-type': 'application/json',
+      'n-token': '35c430ef650b459ba2b9c1409148d929',
+      'request_id': 'c9b9d84a71c0404fa4b5fbcab71b363a',
+      'sec-ch-ua': '"Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'Cookie': 'acw_tc=0a47314717480864781788680e00672d0661eab290838cdcce5932cfb5947f; token=0381E28A719D4DABA0898104248AE231; auth_n=p3qiBagdm3WjuBOG+VuZUyIOgpsFwMSOch8wEX11GBY36j1TkIx9ExqX7Eo51hTf; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22nr_my2b9az6y%22%2C%22first_id%22%3A%2219584822882c1-055782b437e7d7-26011d51-1327104-19584822883c64%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_utm_source%22%3A%22baidu%22%2C%22%24latest_utm_medium%22%3A%22cpc%22%2C%22%24latest_utm_campaign%22%3A%22%E6%96%B0%E7%BA%A2SEM%22%2C%22%24latest_utm_term%22%3A%22%E6%96%B0%E7%BA%A2%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk1ODQ4MjI4ODJjMS0wNTU3ODJiNDM3ZTdkNy0yNjAxMWQ1MS0xMzI3MTA0LTE5NTg0ODIyODgzYzY0IiwiJGlkZW50aXR5X2xvZ2luX2lkIjoibnJfbXkyYjlhejZ5In0%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22nr_my2b9az6y%22%7D%7D'
+    }
+  }
+);
+
+ result = (response.data?.data?.list || []).map(item => ({
+    hotWord: item.hotWord || '',
+    rankDate: item.rankDate || '',
+    noteCount: item.noteCount || 0,
+    dayCount: item.dayCount || 0
+  }));
+  console.log("Fetched hot words: " + JSON.stringify(result));
+  console.log("Fetched hot words: " + result.length);
+
+      } catch (error) {
+        console.error("Failed to fetch hot words:", error);
+        result = [];
+      }
+              break;
             // case "hot_topics":
             //   result = this.getInstance().getTopicRank(params.page);
             //   break;
@@ -519,15 +558,15 @@ async function exampleUsage() {
     // //  API error
     // const searchResults = await factory.search('美食', 'popularity_descending', 1);
     // const searchResults = await factory.search('中医调理',1);
-    const obj =  {"route": "notes_comment_by_next_page", "noteId": "682df0080000000022004187"};
-    while (true) {
-    const searchResults = await APIWrapperFactory.executeRequest( obj, "ccccc");
+    const obj =  {"route": "hot_words", "params": { "page": "1" }};
+    // while (true) {
+    const searchResults = await APIWrapperFactory.executeRequest( obj, "222");
     console.log(" yykai searchResults num: " + searchResults?.length  );
-    if (!searchResults?.length) {
-      break;
-    }
+    // if (!searchResults?.length) {
+    //   break;
+    // }
       
-    }
+    // }
     // console.log('Search Results:', searchResults);
   } catch (error) {
     console.error('Error:', error.message);
