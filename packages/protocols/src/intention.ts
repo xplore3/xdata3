2d0c95f79ec3482e8ed746a9e1816bb94632e012
Xplore3 <Vincent.Rapheal.07@gmail.com>
2025-06-19 15:09:09 +0800

    Update dataProcess Prompt

--- packages/protocols/src/intention.ts
+++ packages/protocols/src/intention.ts
@@ -349,6 +349,7 @@ export class IntentionHandler {
   ): Promise<string> {
     const taskId = message.content?.intention?.taskId;
     const userInput = `根据已有数据，${message.content.text}`;
+    const my_data_source = ApiDb.getUserDataSource(message.userId);
     const attachment = IntentionHandler.getTaskAttachment(taskId);
     const intention_examples = UserKnowledge.getUserIntentionExamples(message.userId);
     const prompt = `
@@ -373,13 +374,14 @@ export class IntentionHandler {
           "intention_options": ["进一步的意图1", "进一步的意图2", "......"],
           "taskId": "${taskId}"
         }
-      (4). 如果用户的需求比较复杂，当前的数据无法满足处理的需求，则需要告知用户缺少什么数据导致无法给出理想结果，并给出intention_options让用户决定是否进一步获取数据。输出结构同(3).
-      (5). 如果用户的输入里，既不包含数据获取需求，也没有明确的数据处理意图，也无其他意图，则参考最近的消息，给出相关的意图选项。输出结构同(3)，其taskId设为空('')。
-      (6). 如果用户的输入（${userInput}）明显与前置描述（${origin_input}）及数据处理无关，则只需给出一个文字回复。
-      (7). 如果用户的输入（{userInput}）与现有数据({attachment})关联不明显，不要强行分析，需要给出类似(2)(3)的结果，其taskId设为空('')。
-      这里(2)(3)(4)(5)输出的JSON，不需要再加其他JSON字段。
-      关于(2)(3)(4)(5)中的analysis_and_question_description，既包含数据分析结果（须以Markdown形式输出），也包含下一步意图的总结性描述，不需要再包含具体意图选项，以免跟intention_options重复。
-      关于(2)(3)(4)(5)中的intention_options，是根据用户输入而得出的选项，以用户明确输入的选项为优先，其次以示例中的选项为优先，
+      (4). 如果用户的需求比较复杂，当前的数据无法满足处理的需求，且可用数据获取API：${my_data_source}中能够得到所需的数据，则输出结构同(1).
+      (5). 如果用户的需求比较复杂，当前的数据无法满足处理的需求，且可用数据获取API：${my_data_source}中能够不能获取所需的数据，则需要告知用户缺少什么数据导致无法给出理想结果，并给出intention_options让用户决定是否进一步获取数据。输出结构同(3).
+      (6). 如果用户的输入里，既不包含数据获取需求，也没有明确的数据处理意图，也无其他意图，则参考最近的消息，给出相关的意图选项。输出结构同(3)，其taskId设为空('')。
+      (7). 如果用户的输入（${userInput}）明显与前置描述（${origin_input}）及数据处理无关，则只需给出一个文字回复。
+      (8). 如果用户的输入（{userInput}）与现有数据({attachment})关联不明显，不要强行分析，需要给出类似(2)(3)的结果，其taskId设为空('')。
+      这里(2)(3)(5)(6)输出的JSON，不需要再加其他JSON字段。
+      关于(2)(3)(5)(6)中的analysis_and_question_description，既包含数据分析结果（须以Markdown形式输出），也包含下一步意图的总结性描述，不需要再包含具体意图选项，以免跟intention_options重复。
+      关于(2)(3)(5)(6)中的intention_options，是根据用户输入而得出的选项，以用户明确输入的选项为优先，其次以示例中的选项为优先，
           且结合用户自身的产品和背景（不要有‘报告生成’这样的宽泛选项，不要有‘分析这些笔记’这样的模糊选项，需要是‘分析这些笔记关于***的特征’）,
           其数量约为5~10个，其可参考的示例如下：【${intention_examples}】。
       -----------------------------
