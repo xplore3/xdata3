0b9471becd8d9734d41d7d972e853d01248379d6
Xplore3 <Vincent.Rapheal.07@gmail.com>
2025-06-29 20:03:22 +0800

    Bug fix

--- packages/protocols/src/apis.ts
+++ packages/protocols/src/apis.ts
@@ -607,7 +607,7 @@ export class ApiDb {
             'userid': $item.user.userid,
             'title': $item.title,
             'desc': $item.desc,
-            'url': [$item.url, 'https://www.xiaohongshu.com/explore/' & $item.id][0],
+            'url': 'https://www.xiaohongshu.com/explore/' & $item.id,
             'type': $item.type,
             'topics': $item.topics,
             'cover': $item.cover,
